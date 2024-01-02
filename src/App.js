53e0a5d96794dd99b31d3ca8531112e3bc31e392
Sulman Ahmed Khan <slmkhanahmed@gmail.com>
2024-01-02 14:43:45 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,73 +1,49 @@
-import { getFinalState } from './processQueue.js';
+import { useState } from 'react';
 
-function increment(n) {
-  return n + 1;
-}
-increment.toString = () => 'n => n+1';
+export default function Form() {
+  const [person, setPerson] = useState({
+    firstName: 'Barbara',
+    lastName: 'Hepworth',
+    email: 'bhepworth@sculpture.com'
+  });
 
-export default function App() {
+  function handleChange(e) {
+    setPerson({
+      ...person,
+      [e.target.name]: e.target.value
+    });
+  }
+
   return (
     <>
-      <TestCase
-        baseState={0}
-        queue={[1, 1, 1]}
-        expected={1}
-      />
-      <hr />
-      <TestCase
-        baseState={0}
-        queue={[
-          increment,
-          increment,
-          increment
-        ]}
-        expected={3}
-      />
-      <hr />
-      <TestCase
-        baseState={0}
-        queue={[
-          5,
-          increment,
-        ]}
-        expected={6}
-      />
-      <hr />
-      <TestCase
-        baseState={0}
-        queue={[
-          5,
-          increment,
-          42,
-        ]}
-        expected={42}
-      />
-    </>
-  );
-}
-
-function TestCase({
-  baseState,
-  queue,
-  expected
-}) {
-  const actual = getFinalState(baseState, queue);
-  return (
-    <>
-      <p>Base state: <b>{baseState}</b></p>
-      <p>Queue: <b>[{queue.join(', ')}]</b></p>
-      <p>Expected result: <b>{expected}</b></p>
-      <p style={{
-        color: actual === expected ?
-          'green' :
-          'red'
-      }}>
-        Your result: <b>{actual}</b>
-        {' '}
-        ({actual === expected ?
-          'correct' :
-          'wrong'
-        })
+      <label>
+        First name:
+        <input
+          name="firstName"
+          value={person.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          name="lastName"
+          value={person.lastName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          name="email"
+          value={person.email}
+          onChange={handleChange}
+        />
+      </label>
+      <p>
+        {person.firstName}{' '}
+        {person.lastName}{' '}
+        ({person.email})
       </p>
     </>
   );
