cdcd5082e65036a2396310e8c6b1809f1d92d694
Salman Khan <slmkhanahmed@gmail.com>
2024-08-09 17:32:27 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Navbar from'./components/Navbar.jsx';
-console.log(hi);
 const App = () => (
     <div>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1320062574413595"
-     crossorigin="anonymous"></script>
      <Navbar />
     </div>
   )
